36940f5d0fc13d26659c46d31fa63e7dc992dc5a
abk <abobkar96@gmail.com>
2023-05-06 09:55:30 +0300

    Media Query Update

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,3 +1,4 @@
+import { Reveal } from "../utils/Reveal";
 import { SectionHeader } from "../utils/SectionHeader";
 import { Project } from "./Project";
 import styles from "./projects.module.scss";
@@ -7,7 +8,9 @@ export const Projects = ({projects}) => {
     <section className="section-wrapper" id="projects">
       <SectionHeader title="Projects" dir="r" />
       
-      <p className={styles.note}>Please note that due to the limitations of the free hosting service used, it might take up to 30 seconds to fully load the first time. I apologize for any inconvenience this may cause and appreciate your patience. Thank you!</p>
+      <Reveal>
+        <p className={styles.note}>Please note that due to the limitations of the free hosting service used, it might take up to 30 seconds to fully load the first time. I apologize for any inconvenience this may cause and appreciate your patience. Thank you!</p>
+      </Reveal>
       <div className={styles.projects}>
         {projects.map((project) => {
           return <Project key={project.title} {...project} />;
